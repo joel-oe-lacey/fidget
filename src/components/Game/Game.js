cf30e1dba00ec504c060a40ccc18d51bbd21c2cb
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-10 10:31:07 -0600

    Create initial Game structure

---
+++ src/components/Game/Game.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Game.scss';
+
+export default class Game extends Component {
+    constructor() {
+        super()
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <section className="Game">
+            </section>
+        )
+    };
+}
+
